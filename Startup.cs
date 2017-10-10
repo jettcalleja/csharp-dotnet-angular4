6d15a032ec48ae43a254cd4e5f0897195559fcab
jettcalleja <jettcalleja@gmail.com>
2017-10-11 03:58:51 +0800

    Update startup to serve angular files

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TodoApi.Models;
 
 namespace csharp_dotnet_angular4
 {
@@ -29,12 +32,24 @@ namespace csharp_dotnet_angular4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // Redirect any nont-API calls to the angular app
+            app.Use(async (context, next) => {
+            await next();
+            if (context.Response.StatusCode == 404 &&
+                !Path.HasExtension(context.Request.Path.Value) &&
+                !context.Request.Path.Value.StartsWith("/api/")) {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
 
-            app.UseMvc();
+            // configures app for usage as API
+            // with default route of '/api/[Controller]'
+            app.UseMvcWithDefaultRoute();
+
+            // configures app to serve the index.html from /wwwroot
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
